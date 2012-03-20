8b6818965d3fbe87995dbe1eae2bf8fc932ded31
Kirill <platonov-kd@ya.ru>
2012-03-20 20:57:35 +0400

    Deleted mc copyright

--- libraries/template.php
+++ libraries/template.php
@@ -14,9 +14,9 @@
  * Нативный шаблонизатор
  */
 class Template {
-        public $vars = array();
-        public $theme = 'default';
-        public $code_added = 0;
+		public $vars = array();
+		public $theme = 'default';
+		public $code_added = 0;
 
 	/**
 	* Construct
@@ -68,7 +68,7 @@ class Template {
 
 		ob_start();
 		include($this->template_file);     
-                $page_content = $this->add_code(ob_get_clean());
+		$page_content = ob_get_clean();
 		return $page_content;
 	}
 
@@ -82,63 +82,19 @@ class Template {
 	/**
 	 * Assign переменных
 	 */
-  	public function assign($param1, $param2 = NULL) {
-  		if(!$param2 && is_array($param1)) {
-  			foreach($param1 AS $key => $value) {
-  				$this->vars[$key] = $value;
-  			}
-  			return TRUE;
-  		}
-  		elseif($param2) {
-  			$this->vars[$param1] = $param2;
-  			return TRUE;
-  		}
-  		return FALSE;
-  	}
-
-  	private function add_code($buffer) {
-  		if(!$this->code_added && strpos($this->theme, 'admin') !== 0) {
-  			if(!class_exists('File_Cache')) a_import('libraries/file_cache');
-  			$file_cache = new File_Cache(ROOT .'cache/file_cache');
-  			$cache_key = 'license_data';
-  			$license_data = $file_cache->get($cache_key, 10800);
-  			$license_data = unserialize($license_data);
-
-  			if(empty($license_data)) {
-  				$license_data = @file_get_contents('http://mobilecms.ru/mobilecms/check_license?domain='. $_SERVER['HTTP_HOST']);
-  				$check_license_data = unserialize($license_data);
-  				if(!isset($check_license_data['licensed'])) $license_data = array();
-  				if(empty($license_data)) {
-  					$license_data = array(
-			     		'licensed' => 1,
-			     		'template' => '{LICENSE_CODE}{LICENSE_PLACE}',
-			     		'license_place' => '</body>',
-			     		'license_code' => '<div>&copy; <a title="MobileCMS - Движок для создания мобильных сайтов, с открытым исходным кодом." href="http://mobilecms.ru">MobileCMS</a></div>'
-			     	);
-
-			     	$license_data = serialize($license_data);
-  				}
-  				$file_cache->set($cache_key, $license_data);
-  				$license_data = unserialize($license_data);
-                                $license_data['license_code'] = '<div>&copy; <a title="MobileCMS - Движок для создания мобильных сайтов, с открытым исходным кодом." href="http://mobilecms.ru">MobileCMS</a></div>';
-  			}
-
-  			if(!$license_data['licensed']) {
-                                if (preg_match('/<!-- copyright -->/i', $buffer)) {
-                                    $buffer = str_replace('<!-- copyright -->', $license_data['license_code'], $buffer);
-                                    $this->code_added = 1;
-                                }
-                                elseif(strstr($buffer, $license_data['license_place'])) {
-	  				$replace = str_replace('{LICENSE_CODE}', $license_data['license_code'], $license_data['template']);
-	  				$replace = str_replace('{LICENSE_PLACE}', $license_data['license_place'], $replace);
-					$buffer = str_ireplace($license_data['license_place'], $replace, $buffer, $count_added);
-					if($count_added > 0) $this->code_added = 1;
-				}
+	public function assign($param1, $param2 = NULL) {
+		if(!$param2 && is_array($param1)) {
+			foreach($param1 AS $key => $value) {
+				$this->vars[$key] = $value;
 			}
-			else $this->code_added = 1;
+			return TRUE;
 		}
-
-		return $buffer;
+		elseif($param2) {
+			$this->vars[$param1] = $param2;
+			return TRUE;
+		}
+		return FALSE;
 	}
 }
+
 ?>
