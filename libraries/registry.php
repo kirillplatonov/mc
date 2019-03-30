2ecb9b981c925c8e08903ce42fbd834a1d3d3e21
KputuK <KpuTuK@CodeMania-Team>
2019-03-30 14:05:14 +0300

    [fix] Исправлен стиль кода

--- libraries/registry.php
+++ libraries/registry.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * MobileCMS
  *
@@ -13,51 +14,53 @@
 /**
  * Статический класс registry
  */
-class Registry
-{
-	/**
-	 * Статическое хранилище для данных
-	 */
-	protected static $store = array();
-     
-	/**
-	 * Защита от создания экземпляров статического класса
-	 */
-	protected function __construct() {}
-	protected function __clone() {}
-     
-	/**
-	 * Проверяет существуют ли данные по ключу
-	 *
-	 * @param string $name
-	 * @return bool
-	 */
-	public static function exists($name) 
-	{
-		return isset(self::$store[$name]);
-	}
-     
-	/**
-	 * Возвращает данные по ключу или null, если не данных нет
-	 *
-	 * @param string $name
-	 * @return unknown
-	 */
-	public static function get($name) 
-	{
-		return (isset(self::$store[$name])) ? self::$store[$name] : null;
-	}
-     
-	/**
-	 * Сохраняет данные по ключу в статическом хранилище
-	 *
-	 * @param string $name
-	 * @param mixed $data
-	 * @return unknown
-	 */
-	public static function set($name, $data) 
-	{
-		return self::$store[$name] = $data;
-	}
+class Registry {
+
+    /**
+     * Статическое хранилище для данных
+     */
+    protected static $store = array();
+
+    /**
+     * Защита от создания экземпляров статического класса
+     */
+    protected function __construct() {
+        
+    }
+
+    protected function __clone() {
+        
+    }
+
+    /**
+     * Проверяет существуют ли данные по ключу
+     *
+     * @param string $name
+     * @return bool
+     */
+    public static function exists($name) {
+        return isset(self::$store[$name]);
+    }
+
+    /**
+     * Возвращает данные по ключу или null, если не данных нет
+     *
+     * @param string $name
+     * @return unknown
+     */
+    public static function get($name) {
+        return (isset(self::$store[$name])) ? self::$store[$name] : null;
+    }
+
+    /**
+     * Сохраняет данные по ключу в статическом хранилище
+     *
+     * @param string $name
+     * @param mixed $data
+     * @return unknown
+     */
+    public static function set($name, $data) {
+        return self::$store[$name] = $data;
+    }
+
 }
-?>
